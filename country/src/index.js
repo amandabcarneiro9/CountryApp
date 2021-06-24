163b3978bc8e811b601ba30cc3da0a3a4436046c
Amanda Carneiro <amandabcarneiro9@gmail.com>
2021-06-24 16:49:06 +0200

    working together

--- country/src/index.js
+++ country/src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <header><h1>Countries of the World</h1></header>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
